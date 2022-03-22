9b120687842ad170d8b6068fa61984fc088f2207
Castle <castlerock88@protonmail.com>
2022-03-22 19:36:05 -0400

    login sending token

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -34,4 +34,42 @@ const postRegister = async (req, res, next) => {
   }
 };
 
-module.exports = { postRegister };
+const postLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      res.status(400);
+      return next(new Error("Please enter all fields"));
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(404);
+      return next(new Error("No user found with that email"));
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      res.status(400);
+      return next(new Error("Invalid credentials"));
+    }
+
+    const token = await user.getSignedToken();
+    if (!token) {
+      res.status(500);
+      return next(new Error("Something went wrong please try again"));
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "successfully logged in, welcome",
+      token,
+    });
+  } catch (err) {
+    res.status(500);
+    next(err);
+  }
+};
+
+module.exports = { postRegister, postLogin };
--- backend/router/userRoutes.js
+++ backend/router/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { postRegister } = require("../controller/userController");
+const { postRegister, postLogin } = require("../controller/userController");
 
 router.post("/register", postRegister);
+
+router.post("/login", postLogin);
 
 module.exports = router;
